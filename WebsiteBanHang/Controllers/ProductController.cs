97f2d92a5a9c81c27e1a49592e0b5ac4ecbfc7f9
DELL <DELL@DESKTOP-KG0GO5U>
2023-10-24 20:24:57 +0700

    Trang Nguoi Dung

--- WebsiteBanHang/Controllers/ProductController.cs
+++ WebsiteBanHang/Controllers/ProductController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanHang.Context;
+using WebsiteBanHang.Models;
 
 namespace WebsiteBanHang.Controllers
 {
     public class ProductController : Controller
     {
+        WebsitebanhangEntities obj = new WebsitebanhangEntities();
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Detail(int id)
         {
-            return View();
+            var objProduct = obj.Products.Where(n => n.id == id).FirstOrDefault();
+            return View(objProduct);
         }
     }
 }
